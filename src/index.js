7791d649ea5f71dd4590646ec6d31e67efa4632d
Ihor-Khomenko <ikhomenko.it@gmail.com>
2020-08-25 20:12:21 +0300

    props learning

--- src/index.js
+++ src/index.js
@@ -3,17 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 
-function Hello(props) {
-  console.log(props);
+function Hello({library, message, number}) {
   return <div>
-    
-    <h1>Welcome to my {props.library}</h1>
-  <p>{props.message}</p>
+    <h1>Welcome to my {library}</h1>
+  <p>{message}</p>
+  <p>
+    {number} total props on site
+    </p>
   </div>;
 }
 
-
 ReactDOM.render(
-  <Hello library="Profile" message="This is my profile" />,
+  <Hello 
+  library="Profile" 
+  message="This is my profile" 
+  number={3}
+  />,
   document.getElementById('root')
 );
